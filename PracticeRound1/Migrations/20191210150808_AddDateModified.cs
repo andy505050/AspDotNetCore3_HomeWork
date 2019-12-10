951eb05267f0e17bb807127acbcc478967f49565
Tom <alex60510@hotmail.com>
2019-12-10 23:50:54 +0800

    8) 請修改 Course, Department, Person 表格，新增 DateModified 欄位(datetime)，並且這三個表格的資料透過 Web API 更新時，都要自動更新該欄位為更新當下的時間 (請新增資料庫移轉紀錄)

---
+++ PracticeRound1/Migrations/20191210150808_AddDateModified.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracticeRound1.Migrations
+{
+    public partial class AddDateModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Person",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Department",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Course",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DepartmentCourseCountVM",
+                columns: table => new
+                {
+                    DepartmentID = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    CourseCount = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartmentCourseCountVM");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Person");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Course");
+        }
+    }
+}
---
+++ PracticeRound1/Models/Partial/Contosouniversitypracticeround1ContextPartial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PracticeRound1.Models
+{
+    public partial class Contosouniversitypracticeround1Context : DbContext
+    {
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entities = ChangeTracker.Entries();
+            foreach (var entry in entities)
+            {
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.CurrentValues.SetValues(new { DateModified = DateTime.Now });
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
