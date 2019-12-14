ab252b38a273f1d9d6e9bb7efbd11dfdf8b1965b
Tom <alex60510@hotmail.com>
2019-12-14 11:05:20 +0800

    新增和設定 Swagger

--- PracticeRound1/Startup.cs
+++ PracticeRound1/Startup.cs
@@ -28,6 +28,8 @@ namespace PracticeRound1 {
             services.AddDbContext<Contosouniversitypracticeround1Context> (options =>
                 options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
 
+            // Register the Swagger services
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,10 @@ namespace PracticeRound1 {
             app.UseRouting ();
 
             app.UseAuthorization ();
+
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseEndpoints (endpoints => {
                 endpoints.MapControllers ();
