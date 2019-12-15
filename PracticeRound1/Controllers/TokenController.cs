f39adb7e889c7f50d3bde1842a6a231268f5a829
Tom <alex60510@hotmail.com>
2019-12-15 12:51:56 +0800

    加入 JWT 發送並可認證和授權

---
+++ PracticeRound1/Controllers/TokenController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeRound1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("text/json")]
+
+    public class TokenController : ControllerBase
+    {
+        [HttpPost("~/signin")]
+        public ActionResult<string> SignIn(LoginViewModel login)
+        {
+            // 以下變數值應該透過 IConfiguration 取得
+            var issuer = "JwtAuthDemo";
+            var signKey = "1234567890123456"; // 請換成至少 16 字元以上的安全亂碼
+            var expires = 30; // 單位: 分鐘
+
+            if (ValidateUser(login))
+            {
+                return JwtHelpers.GenerateToken(issuer, signKey, login.Username, expires);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private bool ValidateUser(LoginViewModel login)
+        {
+            return true; // TODO
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("~/claims")]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+        [Authorize(Roles = "Users,Admin")]
+        [HttpGet("~/username")]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [Authorize(Roles = "Manager")]
+        [HttpGet("~/jwtid")]
+        public IActionResult GetUniqueId()
+        {
+            var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+            return Ok(jti.Value);
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- PracticeRound1/Startup.cs
+++ PracticeRound1/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,29 +13,66 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using PracticeRound1.Models;
 
-namespace PracticeRound1 {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace PracticeRound1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddControllers ();
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
             // using Microsoft.EntityFrameworkCore;
-            services.AddDbContext<Contosouniversitypracticeround1Context> (options =>
-                options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection")));
+            services.AddDbContext<Contosouniversitypracticeround1Context>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             // Register the Swagger services
             services.AddSwaggerDocument();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+                    options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+                        NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                        // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+                        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+                        // 一般我們都會驗證 Issuer
+                        ValidateIssuer = true,
+                        ValidIssuer = "JwtAuthDemo", // "JwtAuthDemo" 應該從 IConfiguration 取得
+
+                        // 若是單一伺服器通常不太需要驗證 Audience
+                        ValidateAudience = false,
+                        //ValidAudience = "JwtAuthDemo", // 不驗證就不需要填寫
+
+                        // 一般我們都會驗證 Token 的有效期間
+                        ValidateLifetime = true,
+
+                        // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+                        ValidateIssuerSigningKey = false,
+
+                        // "1234567890123456" 應該從 IConfiguration 取得
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1234567890123456"))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
             //if (env.IsDevelopment ()) {
             //    app.UseDeveloperExceptionPage ();
             //}
@@ -42,18 +81,21 @@ namespace PracticeRound1 {
                 app.UseExceptionHandler("/Error");
             }
 
-            app.UseHttpsRedirection ();
+            app.UseHttpsRedirection();
 
-            app.UseRouting ();
+            app.UseRouting();
 
-            app.UseAuthorization ();
+            //注意先認證才授權
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             // Register the Swagger generator and the Swagger UI middlewares
             app.UseOpenApi();
             app.UseSwaggerUi3();
 
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllers ();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
             });
         }
     }
